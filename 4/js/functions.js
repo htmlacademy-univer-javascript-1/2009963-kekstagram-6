eec5f37b71763320c387cd41cc88a3a0f0993812
Keks <keks@htmlacademy.ru>
2025-11-29 22:10:48 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/functions.js
@@ -0,0 +1,16 @@
+const isStringShort = (str, length) => str.length <= length;
+
+const isPalindrome = (str) => {
+  const normalizedString = str.replaceAll(' ', '').toLowerCase();
+  return normalizedString === normalizedString.split('').reverse().join('');
+};
+
+const getCounts = (input) => {
+  const result = [];
+  input.toString().split('').forEach((char) => {
+    if (parseInt(char, 10)) {
+      result.push(char);
+    }
+  });
+  return parseInt(result.join(''), 10);
+};
