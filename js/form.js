b9d459784199a0ec6812b3421edd21f37102b918
Daniil Sapockij <asapotsk@bk.ru>
2026-01-07 19:19:51 +0700

    Начало 9.11

--- js/form.js
+++ js/form.js
@@ -2,15 +2,19 @@ const renderForm = () => {
   const formModal = document.querySelector('.img-upload__overlay');
   const input = document.querySelector('.img-upload__input');
   const body = document.querySelector('body');
-
+  const form = document.querySelector('.img-upload__form');
   const closeButton = document.querySelector('.img-upload__cancel');
 
-  // const closeModal = () => {
-  // дописать для esc
-  // formModal.classList.add('hidden');
-  // body.classList.remove('modal-open');
-  // Сбросить форму через reset видимо
-  // };
+  const pristine = new Pristine(form, {
+    classTo: 'setup-wizard-form__element',
+    errorTextParent: 'setup-wizard-form__element',
+    errorTextClass: 'setup-wizard-form__error-text',
+  });
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+  });
 
   const closeModal = (event) => {
     if (event.type !== 'click' && event.key !== 'Escape') {
@@ -19,9 +23,8 @@ const renderForm = () => {
     event.preventDefault();
     formModal.classList.add('hidden');
     document.querySelector('body').classList.remove('modal-open');
-
-    // event.target.removeEventListener('click', closeModal);
-};
+    input.value = '';
+  };
 
   input.addEventListener('change', () => {
     formModal.classList.remove('hidden');
